a7e1f22c8ef5c15e3d8110b231c1df73bc7ecc17
mariamkarray <mariam.karray11@gmail.com>
2020-08-23 16:07:52 +0200

    added options menu

---
+++ Mini Militia Final_1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // create a function that opens different scenes
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
