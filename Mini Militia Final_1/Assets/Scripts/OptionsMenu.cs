8903d7c9e72951f0b7367914430d75db86806ade
mariamkarray <mariam.karray11@gmail.com>
2020-08-25 12:31:47 +0200

    changes

---
+++ Mini Militia Final_1/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    // fullscreen function 
+    public void SetFullScreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
